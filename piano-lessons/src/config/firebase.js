0f96413848775ec1175f6a5f2f2ad852691b3886
thomasam000 <thomasam000@gmail.com>
2022-10-05 21:26:08 -0400

    got basics of vue project with auth done

---
+++ piano-lessons/src/config/firebase.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apiKey: 'AIzaSyC87LG6V9UBasp665NXbYZMJAJYOk9KY0I',
+  authDomain: 'piano-lessons-7969c.firebaseapp.com',
+  projectId: 'piano-lessons-7969c',
+  storageBucket: 'piano-lessons-7969c.appspot.com',
+  messagingSenderId: '691703670902',
+  appId: '1:691703670902:web:20d5dba319fa2e24fb5757',
+  measurementId: 'G-VH9TCRNNFB'
+}
